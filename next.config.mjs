9d93e0e4551a541e5c07426f47d0769b9a0bf8e5
kundanmaster <kundan.masterbazar@gmail.com>
2024-08-28 23:12:27 +0530

    new changes category

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async headers() {
-        return [
+      return [
+        {
+          source: "/apiRoutes/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: process.env.NODE_ENV === "production" ? "https://dignity-sand.vercel.app" : "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
             {
-                // matching all API routes
-                source: "/apiRoutes/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
-};
-
-export default nextConfig;
+              key: "Access-Control-Allow-Headers",
+              value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
